015dcc5d58d2b22f93a9f5ca8fe466ebfbe621d3
HenriqueLR <henrique.lr89@gmail.com>
2015-02-05 00:06:11 -0200

    modify url

--- billtryx/urls.py
+++ billtryx/urls.py
@@ -1,9 +1,19 @@
+#encoding: utf-8
+
+'''
+More informations you can see documentation 
+of the project https://github.com/HenriqueLR/billtryx
+'''
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+
 
 urlpatterns = patterns('',
 
     url(r'^billtryx/', include(admin.site.urls)),
-    url(r'^$', 'seek.views.list_shots', name='list_shots'),
+    url(r'^$', TemplateView.as_view(template_name='list_shots.html'), name='list_shots'),
     url(r'^detail_shots/(?P<pk>\d+)$', 'seek.views.view_detail_shots', name='view_detail_shots'),
 )

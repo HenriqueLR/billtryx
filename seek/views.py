2fbf629496f1ce860ddfbf865ea5aa8d495e1c5b
HenriqueLR <henrique.lr89@gmail.com>
2015-02-04 23:58:25 -0200

    modify view

--- seek/views.py
+++ seek/views.py
@@ -10,31 +10,6 @@ from core.shots import Dribbble
 from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 
-def list_shots(request):
-	try: 
-		page = request.GET.get("page")
-	except ValueError: 
-		page = 1
-
-	try:
-		shots = Dribbble()
-		shots_list = shots.shots('popular', page=page)
-		paginator = shots_list.pop(-1)
-	except:
-		return HttpResponseNotFound('<h1>Page not found</h1>')
-
-	return render(request,'list_shots.html', {'list_shots':shots_list, 'pagination':paginator})
 
 def view_detail_shots(request, *args, **kwargs):
-	try: 
-		id_shots = int(kwargs.get('pk'))
-	except ValueError: 
-		return HttpResponseNotFound('<h1>Page not found</h1>')
-
-	try:
-		shots = Dribbble()
-		shots_list = shots.shot(id_shots)
-	except:
-		return HttpResponseNotFound('<h1>Page not found</h1>')
-
-	return render(request,'detail_shot.html', {'list_shots':shots_list})
+	pass
